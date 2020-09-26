c8f5ee623dc30b14aa49d61ddab216c32d66dcda
Martijn Pieters <mj@zopatista.com>
2020-09-26 18:23:00 +0100

    Clear warnings on Python 3.8 and up

--- src/aiolimiter/compat.py
+++ src/aiolimiter/compat.py
@@ -5,6 +5,7 @@
 # compatibility across Python versions
 import asyncio
 import contextlib
+import sys
 
 try:
     # Python 3.7 or newer
@@ -16,3 +17,15 @@ except AttributeError:  # pragma: no cover
     AsyncContextManagerBase = object  # type: ignore
     current_task = asyncio.Task.current_task
     get_running_loop = asyncio.get_event_loop
+
+
+if sys.version_info < (3, 8):  # pragma: no cover
+    wait_for = asyncio.wait_for
+else:
+    from typing import Any, TypeVar
+
+    _T = TypeVar("_T")
+
+    def wait_for(*args: Any, **kwargs: Any) -> "asyncio.Future[_T]":
+        # loop argument is deprecated, drop it automatically
+        return asyncio.wait_for(*args)
