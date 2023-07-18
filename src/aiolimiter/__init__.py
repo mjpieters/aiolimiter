b1c421ab8111f748dd2e6d40acb322775c2b1ef3
Martijn Pieters <mj@zopatista.com>
2023-07-18 15:58:15 +0100

    Drop support for Python 3.7 (#139)

--- src/aiolimiter/__init__.py
+++ src/aiolimiter/__init__.py
@@ -2,12 +2,7 @@
 # Copyright (c) 2019 Martijn Pieters
 # Licensed under the MIT license as detailed in LICENSE.txt
 
-try:
-    # Python 3.8+
-    from importlib.metadata import version  # type: ignore
-except ImportError:
-    # Python 3.7
-    from importlib_metadata import version  # type: ignore
+from importlib.metadata import version  # type: ignore
 
 from .leakybucket import AsyncLimiter
 
