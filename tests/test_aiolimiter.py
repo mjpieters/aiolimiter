1341be59be117030143e884bb6ae9a4465d3fa79
Martijn Pieters <mj@zopatista.com>
2025-06-29 19:07:51 +0100

    chore: Modernize development toolchain (#313)
    - Use Github Actions for all CI steps
    - Switch to pyright, ruff, uv and task
    - Handle version dynamically
    - Replace dependabot with renovate

--- tests/test_aiolimiter.py
+++ tests/test_aiolimiter.py
@@ -3,54 +3,43 @@
 # Licensed under the MIT license as detailed in LICENSE.txt
 
 import asyncio
-from pathlib import Path
+from collections.abc import Collection, Coroutine
+from typing import Any, Callable, TypeVar
 from unittest import mock
 
 import pytest
-
-try:
-    import tomllib
-
-    def toml_load_path(path: Path):
-        with open(path, "rb") as f:
-            data = tomllib.load(f)
-        return data
-
-except ImportError:
-    import toml
-
-    def toml_load_path(path: Path):
-        return toml.load(path)
-
+from dunamai import Version
+from typing_extensions import Self
 
 from aiolimiter import AsyncLimiter
 
 # max number of wait_for rounds when waiting for all events to settle
 MAX_WAIT_FOR_ITER = 5
 
+T = TypeVar("T")
 
-def test_version():
+
+def test_version() -> None:
     # the version is taken from metadata
     from aiolimiter import __version__
 
     assert __version__
 
-    pyproject = Path(__file__).resolve().parent.parent / "pyproject.toml"
-    metadata = toml_load_path(pyproject)["tool"]["poetry"]
+    vcs_version = Version.from_git().serialize()
 
     # pyproject bumps to -alpha.0, -beta.1, etc., but releases a0, b1
     # We don't really need to care about those, just verify that sorta
     # the right version is used.
-    assert __version__.startswith(metadata["version"].partition("-")[0])
+    assert __version__ == vcs_version
 
 
-def test_attributes():
+def test_attributes() -> None:
     limiter = AsyncLimiter(42, 81)
     assert limiter.max_rate == 42
     assert limiter.time_period == 81
 
 
-async def test_has_capacity():
+async def test_has_capacity() -> None:
     limiter = AsyncLimiter(1)
     assert limiter.has_capacity()
     assert not limiter.has_capacity(42)
@@ -59,26 +48,29 @@ async def test_has_capacity():
     assert not limiter.has_capacity()
 
 
-async def test_over_acquire():
+async def test_over_acquire() -> None:
     limiter = AsyncLimiter(1)
     with pytest.raises(ValueError):
         await limiter.acquire(42)
 
 
-async def acquire_task(limiter):
+async def acquire_task(limiter: AsyncLimiter) -> None:
     await limiter.acquire()
 
 
-async def async_contextmanager_task(limiter):
+async def async_contextmanager_task(limiter: AsyncLimiter) -> None:
     async with limiter:
         pass
 
 
-async def wait_for_n_done(tasks, n):
-    """Wait for n (or more) tasks to have completed"""
+async def wait_for_n_done(
+    tasks: Collection[asyncio.Task[T]], n: int
+) -> set[asyncio.Task[T]]:
+    """Wait for n (or more) tasks to have completed."""
     iteration = 0
     remainder = len(tasks) - n
     pending_count = len(tasks)
+    pending: set[asyncio.Task[T]] = set()
     while iteration <= MAX_WAIT_FOR_ITER:
         iteration += 1
         _, pending = await asyncio.wait(
@@ -94,24 +86,26 @@ async def wait_for_n_done(tasks, n):
 
 
 class MockLoopTime:
-    def __init__(self):
+    def __init__(self) -> None:
         self.current_time = 0
         event_loop = asyncio.get_running_loop()
         self.patch = mock.patch.object(event_loop, "time", self.mocked_time)
 
-    def mocked_time(self):
+    def mocked_time(self) -> int:
         return self.current_time
 
-    def __enter__(self):
+    def __enter__(self) -> Self:
         self.patch.start()
         return self
 
-    def __exit__(self, *_):
+    def __exit__(self, *_: object) -> None:
         self.patch.stop()
 
 
 @pytest.mark.parametrize("task", [acquire_task, async_contextmanager_task])
-async def test_acquire(task):
+async def test_acquire(
+    task: Callable[[AsyncLimiter], Coroutine[Any, Any, None]],
+) -> None:
     # capacity released every 2 seconds
     limiter = AsyncLimiter(5, 10)
 
@@ -136,7 +130,7 @@ async def test_acquire(task):
         assert len(pending) == 0
 
 
-async def test_acquire_wait_time():
+async def test_acquire_wait_time() -> None:
     limiter = AsyncLimiter(3, 3)
 
     with MockLoopTime() as mocked_time:
@@ -153,7 +147,7 @@ async def test_acquire_wait_time():
         assert not pending
 
 
-async def test_decreasing_acquire():
+async def test_decreasing_acquire() -> None:
     limiter = AsyncLimiter(3, 3)
     with MockLoopTime() as mocked_time:
         # Fill the bucket with an amount of 1
@@ -177,7 +171,7 @@ async def test_decreasing_acquire():
         assert not pending
 
 
-async def test_task_cancelled():
+async def test_task_cancelled() -> None:
     limiter = AsyncLimiter(3, 3)
     with MockLoopTime() as mocked_time:
         # Fill the bucket with an amount of 1
@@ -196,7 +190,7 @@ async def test_task_cancelled():
         assert not pending
 
 
-def test_multiple_loops():
+def test_multiple_loops() -> None:
     limiter = AsyncLimiter(1, 1)
 
     async def task():
